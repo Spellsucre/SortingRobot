0cb279efd1a9e3638514587e9cc2c956e1374e79
Vinson <paul.vinson.etu@upmc.fr>
2018-03-06 18:24:01 +0100

    Supression des inutiles

--- SortingRobot/Robot_5_5_24_1.sol
+++
@@ -1,2 +0,0 @@
-5 5 24 1
-S D R R S R S D L L L S D R R S U U L L S U R R R R S L L L S R S D D S D L L S U U U R R R S D D D D L L S R R R p 0
--- SortingRobot/Robot_5_5_5_1_2.sol
+++
@@ -1,2 +0,0 @@
-5 5 5 1
-S D D D D S U U U S U R R R S L L S L S D D S D D R R R R S L S U U U L S D D S U L L S D D R S U U R R R S D D L L S U U U R R S D D D L L L S 0
